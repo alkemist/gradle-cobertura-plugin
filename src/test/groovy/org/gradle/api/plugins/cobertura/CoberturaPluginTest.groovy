0f816e3993bbd154e81dae6a87c4d69463fecd0f
Luke Daley <ld@ldaley.com>
2012-09-30 14:23:46 +0100

    Added a test for test tasks being extended.

--- src/test/groovy/org/gradle/api/plugins/cobertura/CoberturaPluginTest.groovy
+++ src/test/groovy/org/gradle/api/plugins/cobertura/CoberturaPluginTest.groovy
@@ -3,6 +3,7 @@ package org.gradle.api.plugins.cobertura
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
+import org.gradle.api.plugins.JavaPlugin
 
 class CoberturaPluginTest extends Specification {
     Project project = ProjectBuilder.builder().build()
@@ -14,5 +15,13 @@ class CoberturaPluginTest extends Specification {
     def "extensions are installed"() {
         expect:
         project.extensions.getByName("cobertura") instanceof CoberturaPluginExtension
+    }
+
+    def "when java plugin is applied test task is extended"() {
+        when:
+        project.apply(plugin: JavaPlugin)
+
+        then:
+        project.tasks.test.extensions.getByName("cobertura") instanceof CoberturaTestTaskExtension
     }
 }
